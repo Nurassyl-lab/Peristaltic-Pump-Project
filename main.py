9c58299a39345f2b8c351dca7b69ded042780c3b
Nurassyl-lab <59994067+Nurassyl-lab@users.noreply.github.com>
2020-07-03 15:33:39 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,22 @@ worksheet = workbook.sheet_by_name('Sheet1')
 
 time = [worksheet.cell_value(i, 0) for i in range (1709)]
 capacitance_all = [worksheet.cell_value(i, 1) for i in range (1709)]
-
+plt.figure(1)
+plt.title("FULL TIME")
 plt.plot(time , capacitance_all) 
-
 plt.show()
+
+plt.figure(2)
+plt.title("ONLY 40 SECONDS")
+time = [worksheet.cell_value(i, 0) for i in range (373)]
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (373)]
+plt.plot(time , capacitance_all) 
+plt.show()
+
+plt.figure(3)
+plt.title("ONLY 12 SECONDS WHEN PUMP IS OFF")
+time = [worksheet.cell_value(i, 0) for i in range (116)]
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (116)]
+plt.plot(time , capacitance_all) 
+plt.show()
+
