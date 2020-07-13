6fee605993005a844b589cdf7865700435b6a463
Nurassyl-lab <59994067+Nurassyl-lab@users.noreply.github.com>
2020-07-13 18:07:54 +0800

    Add files via upload

--- current.py
+++ current.py
@@ -3,7 +3,6 @@ import numpy as np
 import xlrd
 from numpy.fft import fft, fftshift, ifft
 from scipy.signal import butter,filtfilt
-from scipy import signal
 
 def butter_lowpass_filter(data, cutoff, fs, order):
     normal_cutoff = cutoff / nyq
@@ -12,23 +11,12 @@ def butter_lowpass_filter(data, cutoff, fs, order):
     y = filtfilt(b, a, data)
     return y
 
-
-
-# def butter_lowpass_filter(data, cutoff, fs, order):
-#     normal_cutoff = cutoff / nyq
-#     # Get the filter coefficients 
-#     b, a = butter(order, normal_cutoff, btype='low', analog=False)
-#     y = filtfilt(b, a, data)
-#     return y
-
 workbook = xlrd.open_workbook('5RPM.xlsx', on_demand = True)
 worksheet = workbook.sheet_by_name('Sheet1') 
 
+#remove comment if you want to plot full signal at 5 RPM
 # time = [worksheet.cell_value(i, 0) for i in range (679)]
 # capacitance_all = [worksheet.cell_value(i, 1) for i in range (679)]
-
-cap = []
-
 # plt.figure(1)
 # plt.title("5 RPM")
 # plt.ylabel('Amplitude')
@@ -36,8 +24,10 @@ cap = []
 # plt.plot(capacitance_all) 
 # plt.show()
 
+cap = []
 #here I'm making a window(24sec - 34sec)
 time = [worksheet.cell_value(i, 0) for i in range (220, 430, 1)]
+
 #windows
 #I did it very stupid, because I can't get used to for loop in python
 
@@ -114,7 +104,10 @@ fs = 10
 nyq = 0.5 * fs
 cutoff = 1.0
 order = 2
-y = butter_lowpass_filter(cap, cutoff, fs, order)
+
+
+# y = cap#comment this line if you want to plot the signal with noise
+y = butter_lowpass_filter(cap, cutoff, fs, order)#comment this line if you want to plot signal without noise
 
 #Before FFT
 plt.figure(0)
