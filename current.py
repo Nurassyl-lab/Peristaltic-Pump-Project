bda641d283d427a091411c81594aebf8f175b212
Nurassyl-lab <59994067+Nurassyl-lab@users.noreply.github.com>
2020-07-13 17:25:49 +0800

    Add files via upload

--- current.py
+++ current.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import xlrd
-from numpy.fft import fft, fftshift
+from numpy.fft import fft, fftshift, ifft
 from scipy.signal import butter,filtfilt
 from scipy import signal
+
+def butter_lowpass_filter(data, cutoff, fs, order):
+    normal_cutoff = cutoff / nyq
+    # Get the filter coefficients 
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+
 
 # def butter_lowpass_filter(data, cutoff, fs, order):
 #     normal_cutoff = cutoff / nyq
@@ -15,8 +24,8 @@ from scipy import signal
 workbook = xlrd.open_workbook('5RPM.xlsx', on_demand = True)
 worksheet = workbook.sheet_by_name('Sheet1') 
 
-time = [worksheet.cell_value(i, 0) for i in range (679)]
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (679)]
+# time = [worksheet.cell_value(i, 0) for i in range (679)]
+# capacitance_all = [worksheet.cell_value(i, 1) for i in range (679)]
 
 cap = []
 
@@ -28,14 +37,13 @@ cap = []
 # plt.show()
 
 #here I'm making a window(24sec - 34sec)
-# time = [worksheet.cell_value(i, 0) for i in range (220, 330, 1)]
-
+time = [worksheet.cell_value(i, 0) for i in range (220, 430, 1)]
 #windows
 #I did it very stupid, because I can't get used to for loop in python
 
 #1
 
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (220, 240, 1)]#20
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (220, 250, 1)]
 capacitance_all = np.array(capacitance_all)
 mean = np.mean(capacitance_all)
 capacitance_all = capacitance_all - mean
@@ -43,7 +51,7 @@ cap.append(capacitance_all)
 mean = 0
 
 #2
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (235, 255, 1)]#20
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (250, 280, 1)]
 capacitance_all = np.array(capacitance_all)
 capacitance_all = np.array(capacitance_all)
 mean = np.mean(capacitance_all)
@@ -52,7 +60,7 @@ cap.append(capacitance_all)
 mean = 0
 
 #3
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (250, 270, 1)]#20
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (280, 310, 1)]
 capacitance_all = np.array(capacitance_all)
 capacitance_all = np.array(capacitance_all)
 mean = np.mean(capacitance_all)
@@ -61,7 +69,7 @@ cap.append(capacitance_all)
 mean = 0
 
 #4
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (265, 285, 1)]#20
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (310, 340, 1)]
 capacitance_all = np.array(capacitance_all)
 capacitance_all = np.array(capacitance_all)
 mean = np.mean(capacitance_all)
@@ -70,7 +78,7 @@ cap.append(capacitance_all)
 mean = 0
 
 #5
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (280, 300, 1)]#20
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (340, 370, 1)]
 capacitance_all = np.array(capacitance_all)
 capacitance_all = np.array(capacitance_all)
 mean = np.mean(capacitance_all)
@@ -79,7 +87,7 @@ cap.append(capacitance_all)
 mean = 0
 
 #6
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (295, 315, 1)]#20
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (370, 400, 1)]
 capacitance_all = np.array(capacitance_all)
 capacitance_all = np.array(capacitance_all)
 mean = np.mean(capacitance_all)
@@ -88,7 +96,7 @@ cap.append(capacitance_all)
 mean = 0
 
 #7
-capacitance_all = [worksheet.cell_value(i, 1) for i in range (310, 330, 1)]#20
+capacitance_all = [worksheet.cell_value(i, 1) for i in range (400, 430, 1)]
 capacitance_all = np.array(capacitance_all)
 capacitance_all = np.array(capacitance_all) 
 mean = np.mean(capacitance_all)
@@ -96,19 +104,42 @@ capacitance_all = capacitance_all - mean
 cap.append(capacitance_all)
 mean = 0
 
-#FFT
 cap = np.array(cap).flatten()
+#scaled the data
 m = max(cap)
 cap = cap / m
-cap = fft(cap)
-cap = fftshift(cap)
-cap = abs(cap)
+# plt.plot(time, cap)
 
+fs = 10
+nyq = 0.5 * fs
+cutoff = 1.0
+order = 2
+y = butter_lowpass_filter(cap, cutoff, fs, order)
+
+#Before FFT
+plt.figure(0)
+plt.title("Time domain")
+plt.ylabel('Frequency')
+plt.xlabel('Time')
+plt.plot(time, y) 
+plt.show()
+
+#FFT
+y = fft(y)
+y = fftshift(y)
+y = abs(y)
 plt.figure(1)
 plt.title("FFT")
 plt.ylabel('Power')
 plt.xlabel('Frequency')
-plt.plot(cap) 
+plt.plot(y) 
 plt.show()
 
+plt.figure(2)
+plt.title("IFFT (impuls response LTI) h(t)")#if the x(y) (input) is impuls, out will be impulse response h(t)
+y = ifft(y)
+# plt.ylabel('Power')
+plt.xlabel('TIME')
+plt.plot(y) 
+plt.show()
 #working on low pass filter
